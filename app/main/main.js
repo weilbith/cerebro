92d0e0d124a00f594b7fc34906087a557e75c6ec
Thore Hans-Jörg Weilbier <thore.h.weilbier@campus.tu-berlin.de>
2017-12-12 18:31:53 +0100

    Remove obsolte function by last commit.
    Accidently the method 'applyCustomTheme' was defined two times.
    Now using the definition from the lib/themizer.

--- app/main/main.js
+++ app/main/main.js
@@ -6,6 +6,7 @@ import initializePlugins from 'lib/initializePlugins'
 import { on } from 'lib/rpc'
 import { updateTerm } from './actions/search'
 import config from '../lib/config'
+import themizer from '../lib/themizer'
 import store from './store'
 import Cerebro from './components/Cerebro'
 import './css/global.css'
@@ -29,20 +30,8 @@ const changeTheme = (src) => {
 changeTheme(config.get('theme'))
 
 
-/**
- * If the user set to use a custom theme, this method apply this.
- * In fact it overwrite the CSS variables, used by the themes, with the color values, defined in the settings by the user.
- */
-const applyCustomTheme = () => {
-    // Only apply if the user required this in the settings.
-    if (config.get('customTheme')) {
-        // Overwrite the theme css variables by the settings.
-        document.documentElement.style.setProperty('--main-background-color', config.get('customTheme_bgColor'))
-    }
-}
-
 // Apply the custom theme if required.
-applyCustomTheme()
+themizer.applyCustomTheme()
 
 // Render main container
 ReactDOM.render(
