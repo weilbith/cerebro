b78a95bf16e1dd1606e6806410a7636e445d556a
Thore Hans-Jörg Weilbier <thore.h.weilbier@campus.tu-berlin.de>
2017-12-12 18:03:21 +0100

    Add function that apply the custom theme.
    When the application is started, it will be checked if the user want a
    custom theme.
    If so, it takes the custom theme values and overwrite the CSS variables
    from the theme by them.

--- app/main/main.js
+++ app/main/main.js
@@ -28,6 +28,22 @@ const changeTheme = (src) => {
 // Set theme from config
 changeTheme(config.get('theme'))
 
+
+/**
+ * If the user set to use a custom theme, this method apply this.
+ * In fact it overwrite the CSS variables, used by the themes, with the color values, defined in the settings by the user.
+ */
+const applyCustomTheme = () => {
+    // Only apply if the user required this in the settings.
+    if (config.get('customTheme')) {
+        // Overwrite the theme css variables by the settings.
+        document.documentElement.style.setProperty('--main-background-color', config.get('customTheme_bgColor'))
+    }
+}
+
+// Apply the custom theme if required.
+applyCustomTheme()
+
 // Render main container
 ReactDOM.render(
   <Provider store={store}>
